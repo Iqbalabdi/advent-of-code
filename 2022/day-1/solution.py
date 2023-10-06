70e9e10ebdf3839df131dd34f9b07cf91221e3b0
Iqbalabdi <iqbalabdi07@gmail.com>
2023-10-06 22:02:14 +0700

    feat: add solution

---
+++ 2022/day-1/solution.py
@@ -0,0 +1,17 @@
+
+file = open("input.txt", "r")
+count = 0
+arr = []
+for line in file:
+    if line != "\n":
+        count += int(line.rstrip())
+    else:
+        arr.append(count)
+        count = 0
+
+### Part 1
+print(max(count))
+
+### Part 2
+arr.sort(reverse=True)
+print(arr[0]+arr[1]+arr[2])
